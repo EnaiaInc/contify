7cf987820136429d6dc1d845389916daa8b6e68b
Ricardo Ramos <rcdo.ramos@gmail.com>
2023-01-06 12:55:27 -0300

    Fix wrong assertions in tests

--- test/contify_test.exs
+++ test/contify_test.exs
@@ -5,14 +5,37 @@ defmodule ContifyTest do
     setup do
       Tesla.Mock.mock(fn
         %{method: :post} ->
-          {:ok, %ContifyAPI.Model.WebhookGetRequest{}}
+          %Tesla.Env{status: 200, body: ~s({
+            "id": 522,
+            "name": "MyWebhook",
+            "client_url": "https://www.apple.com/in/",
+            "header_params": {
+              "name": "",
+              "secret": ""
+            },
+            "query_params": {
+              "keyword": "",
+              "topic_id": "",
+              "source_id": "56",
+              "channel_id": "",
+              "company_id": "",
+              "industry_id": "",
+              "language_id": "",
+              "location_id": "",
+              "advanced_query": "",
+              "content_type_id": "",
+              "custom_topic_id": ""
+            },
+            "created_on": "20 Oct, 2022",
+            "updated_on": "20 Oct, 2022"
+          })}
       end)
 
       :ok
     end
 
     test "subscribe to webhook" do
-      webhook = %{
+      params = %{
         name: "some name",
         url: "some endpoint",
         headerName: "",
@@ -30,63 +53,65 @@ defmodule ContifyTest do
         advancedQuery: ""
       }
 
-      assert Contify.subscribe_to_webhook(webhook)
-      {:ok, %ContifyAPI.Model.WebhookGetRequest{}}
+      assert {:ok, %ContifyAPI.Model.WebhooksResponse{id: 522}} =
+               Contify.subscribe_to_webhook(params)
     end
   end
 
   describe "search_company" do
     setup do
       Tesla.Mock.mock(fn
-        %{method: :post} ->
-          {:ok,
-           %ContifyAPI.Model.SearchCompanyGet200Response{
-             count: 30,
-             next: "?page=2",
-             previous: nil,
-             results: [
-               %ContifyAPI.Model.Company{
-                 id: 697,
-                 name: "Microsoft Corporation",
-                 url: "https://www.microsoft.com/",
-                 hq_country: "United States",
-                 hq_state: "Washington State",
-                 logo: "//112233.contify.com/images/company/logo/microsoft-28536.PNG",
-                 twitter: "https://twitter.com/microsoft",
-                 youtube: "https://www.youtube.com/MICROSOFT",
-                 facebook: "https://www.facebook.com/MicrosoftIndia",
-                 google: "https://plus.google.com/+Microsoft",
-                 linkedin: "https://www.linkedin.com/company/microsoft/"
+        %{method: :get} ->
+          %Tesla.Env{status: 200, body: ~s({ 
+             "count": 30,
+             "next": "?page=2",
+             "previous": null,
+             "results": [
+               {
+                 "id": 697,
+                 "name": "Microsoft Corporation",
+                 "url": "https://www.microsoft.com/",
+                 "hq_country": "United States",
+                 "hq_state": "Washington State",
+                 "logo": "//112233.contify.com/images/company/logo/microsoft-28536.PNG",
+                 "twitter": "https://twitter.com/microsoft",
+                 "youtube": "https://www.youtube.com/MICROSOFT",
+                 "facebook": "https://www.facebook.com/MicrosoftIndia",
+                 "google": "https://plus.google.com/+Microsoft",
+                 "linkedin": "https://www.linkedin.com/company/microsoft/"
                }
              ]
-           }}
+           })}
       end)
 
       :ok
     end
 
     test "search_company return results for searched company" do
-      assert Contify.search_company(name: "Patagonia")
-      {:ok, %ContifyAPI.Model.SearchCompanyGet200Response{}}
+      assert {:ok,
+              %ContifyAPI.Model.SearchCompanyGet200Response{
+                results: [%ContifyAPI.Model.Company{id: 697}]
+              }} = Contify.search_company(name: "Patagonia")
     end
+  end
 
+  describe "request_company" do
     setup do
       Tesla.Mock.mock(fn
-        %{method: :post} ->
-          {:ok,
-           %ContifyAPI.Model.RequestCompanyGet200Response{
-             message: "Your request was successful for the company.",
-             name: "Microsoft",
-             url: "microsoft.com"
-           }}
+        %{method: :get} ->
+          %Tesla.Env{status: 200, body: ~s({
+             "message": "Your request was successful for the company.",
+             "name": "Microsoft",
+             "url": "microsoft.com"
+           })}
       end)
 
       :ok
     end
 
     test "request a company" do
-      assert Contify.request_company("Patagonia", "patagonia.com")
-      {:ok, %ContifyAPI.Model.SearchCompanyGet200Response{}}
+      assert {:ok, %ContifyAPI.Model.RequestCompanyGet200Response{}} =
+               Contify.request_company("Patagonia", "patagonia.com")
     end
   end
 end
